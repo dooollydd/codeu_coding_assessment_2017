424e9477cd90eaba9640df75a4ddaacbb7aacfcc
dooollydd <jiahed2@illinois.edu>
2017-04-28 01:52:24 -0500

    jsonparser submission

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -18,37 +18,78 @@ import java.util.Collection;
 
 final class MyJSON implements JSON {
 
-  @Override
-  public JSON getObject(String name) {
-    // TODO: implement this
-    return null;
-  }
+    // list object that fills in the "object" part in the map - unused?
+    private List<HashMap> object = new ArrayList<Hashmap>();
 
-  @Override
-  public JSON setObject(String name, JSON value) {
-    // TODO: implement this
-    return this;
-  }
+    // to memo all the data with a nesting structure
+    // it is unknown type because it might be either hashmap or string
+    private HashMap<String, ? extends Object> memo = new HashMap<String, Object>();
 
-  @Override
-  public String getString(String name) {
-    // TODO: implement this
-    return null;
-  }
+    /**
+     * Get the value of the nested object with the given name.
+     * If there is no nested object with that name, the method will return null.
+     *
+     * @param name name of the object
+     * @return
+     */
+    @Override
+    public JSON getObject(String name) {
+        // TODO: implement this
+        if (!memo.containsKey(name))
+            return null; // return null if no key has name "name"
 
-  @Override
-  public JSON setString(String name, String value) {
-    // TODO: implement this
-    return this;
-  }
+        else
+            return memo.get(name); // else return the object corresponding to "name"
+    }
 
-  @Override
-  public void getObjects(Collection<String> names) {
-    // TODO: implement this
-  }
+    @Override
+    public JSON setObject(String name, JSON value) {
+        // TODO: implement this
 
-  @Override
-  public void getStrings(Collection<String> names) {
-    // TODO: implement this
-  }
+        memo.put(name, value); // put the set in; will be overwritten if existed
+        return this;
+    }
+
+    @Override
+    public String getString(String name) {
+        // TODO: implement this
+
+        if (!memo.containsKey(name))
+            return null; // return null if no key has name "name"
+        else
+            return memo.get(name); // else return the string corresponding to "name"
+    }
+
+    @Override
+    public JSON setString(String name, String value) {
+        // TODO: implement this
+
+        memo.put(name, value); // put the set in; will be overwritten if existed
+        return this;
+    }
+
+    @Override
+    public void getObjects(Collection<String> names) {
+        // TODO: implement this
+
+        for (String key : memo.keySet()) {
+            // add every key in the map into names if the object is a map (not string)
+            if (!memo.get(key) instanceof String) {
+                names.add(key);
+            }
+        }
+
+    }
+
+    @Override
+    public void getStrings(Collection<String> names) {
+        // TODO: implement this
+        for (String key : memo.keySet()) {
+            // add every key in the map into names if the object is a string
+            if (memo.get(key) instanceof String) {
+                names.add(key);
+            }
+        }
+
+    }
 }
--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -18,9 +18,78 @@ import java.io.IOException;
 
 final class MyJSONParser implements JSONParser {
 
-  @Override
-  public JSON parse(String in) throws IOException {
-    // TODO: implement this
-    return new MyJSON();
-  }
+    @Override
+    public JSON parse(String in) throws IOException {
+        // TODO: implement this
+        MyJSON jsonObject = new MyJSON();
+
+        int l = 0; // start from the one after "{"
+        int r = 1;
+
+        HashMap<String, ? extends Object> input = formMap(in, l, r);
+
+        jsonObject.memo.putAll(input);
+    }
+
+    /**
+     * split the input string in order
+     * and extract the first part of the top level of the string
+     *
+     * @param input string to parse
+     * @param l     left index of the substring
+     * @param r     right index of the substring
+     * @return A String that is extracted from the original string
+     */
+    public String findPart(String input, int l, int r) throws IOException {
+
+        int count = 0;
+        for (char c : input) {
+            if (c == "{")
+                count++;
+            if (c == "}")
+                count--;
+
+            // count should already be 1 at the beginning cuz the first char is "{"
+            // when count == 0, the index is on the top level in the string
+            if (count == 0) {
+                r = input.indexOf(c);
+            }
+        }
+        return input.substring(l, r - l + 2);
+    }
+
+    /**
+     * form a map structure with the given string
+     *
+     * @param input string to parse and put into a map
+     * @param l     left index of the subpart
+     * @param r     right index of the subpart
+     * @return an hashmap object
+     */
+    public HashMap<String, HashMap> formMap(String input, int l, int r) throws IOException {
+        HashMap<String, ? extends Object> ret = new HashMap<String, Object>();
+
+        // when the index of "{" < 0, it means that no more nesting
+        if (input.indexOf("{") < 0) {
+            String[] record = input.split(":");
+            ret.put(record[0], record[1]);
+        } else {
+            String str = input;
+
+            // keep looping as long as the string is not empty
+            while (l != str.size() - 1) {
+
+                // extract the first part of the top level of the string
+                String subpart = findPart(input, l, r);
+
+                // using recursion to place multiple nesting in the hashmap
+                for (String s : subpart.split(":")) {
+                    ret.put(s, formMap(subpart, 1, 0));
+                }
+
+                l = r + 1; // start again from the remaining part
+            }
+        }
+        return ret;
+    }
 }
